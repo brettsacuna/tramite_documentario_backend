1c2950c584a38316f3cd27919407e0aceacd85a1
DAVIS BRETTS ACUÑA CORDOVA <dacucordova@gmail.com>
2016-11-28 03:24:24 -0500

    Mejoras

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -9,7 +9,19 @@
 	        $this->load->model(array('Usuario_model'));
 	    }
 
+		public function index_get() {
+			$this->response($this->Usuario_model->get_usuarios(), REST_Controller::HTTP_OK);
+		}
+
 	    public function index_post() {
 	    	$this->response($this->Usuario_model->check_login($this->input->post()), REST_Controller::HTTP_OK);
 	    }
+
+		public function nuevo_post() {
+			$this->response($this->Usuario_model->new_usuario($this->input->post()), REST_Controller::HTTP_OK);
+		}
+
+		public function index_delete() {
+			$this->response($this->Usuario_model->delete_usuario($this->input->get('usuario')), REST_Controller::HTTP_OK);
+		}
 	}
--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -18,4 +18,51 @@
                 return array("estado" => 0, "usuario" => null);
             }
         }
+
+        public function get_usuarios() {
+            $this->db->select("usuario.usuario_id, usuario.usuario, usuario.tipo_usuario, usuario.estado");
+            return $this->db->get_where('usuario', array('usuario.tipo_usuario' => 1))->result();
+        }
+
+        public function new_usuario($usuario) {
+            try {
+                $this->db->trans_begin();
+
+                $this->db->insert('usuario', $usuario);
+
+                $usuario_insertado = $this->db->insert_id();
+
+                if ($this->db->trans_status() === false){
+                    $this->db->trans_rollback();
+
+                    return array('estado' => 0, 'usuario' => null);
+                } else {
+                    $this->db->trans_commit();
+
+                    return array('estado' => 1, 'usuario' => $usuario_insertado);
+                }
+            } catch (Exception $e) {
+                return array('estado' => 0, 'usuario' => null);
+            }
+        }
+
+        public function delete_usuario($usuario) {
+            try {
+                $this->db->trans_begin();
+
+                $this->db->delete('usuario', array('usuario_id' => $usuario));
+
+                if ($this->db->trans_status() === false){
+                    $this->db->trans_rollback();
+
+                    return array('estado' => 0, 'usuario' => null);
+                } else {
+                    $this->db->trans_commit();
+
+                    return array('estado' => 1, 'usuario' => $usuario);
+                }
+            } catch (Exception $e) {
+                return array('estado' => 0, 'usuario' => null);
+            }
+        }
     }
